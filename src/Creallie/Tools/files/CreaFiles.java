41124d39884960beadfbd7b61a6caa46ec94c394
William Norman <lordcrekit@gmail.com>
2015-08-28 15:56:39 -0400

    Updated the CreaFiles class to allow copy and deletion of entire directories.

--- src/Creallie/Tools/files/CreaFiles.java
+++ src/Creallie/Tools/files/CreaFiles.java
@@ -24,6 +24,8 @@
 package Creallie.Tools.files;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 /**
  *
@@ -32,26 +34,48 @@ import java.io.File;
 public class CreaFiles {
 
     /*
-     * ================================================ MEMBER VARIABLES ================================================
-     */
-
-    /*
-     * ================================================== CONSTRUCTORS ==================================================
-     */
-    /**
-     * Default constructor for CreaFiles.
-     */
-    public CreaFiles() {
-    }
-
-    /*
      * ================================================ PRIMARY FUNCTIONS ===============================================
      */
     /**
-     * 
-     * @return 
+     * Copies the given directory to the desired destination. The directories name will be overwritten with the destinations name, but all files inside of the
+     * directory will maintain their names and structure.
+     *
+     * @param directory <code>{@link File }</code>: The directory you wish to copy.
+     * @param destination <code>{@link File }</code>: The destination you wish to copy to.
+     * @throws IOException If something goes wrong trying to copy the files.
      */
-    public static File getAppData() {
+    public static void copyDirectory( File directory, File destination ) throws IOException {
+        if ( directory.isDirectory() ) {
+            Files.createDirectories(destination.toPath());
+            for ( File i : directory.listFiles() )
+                copyDirectory(i, new File(destination + File.separator + i.getName()));
+        } else
+            Files.copy(directory.toPath(), destination.toPath());
+    }
+
+    /**
+     * Deletes an entire directory and all it's sub folders and files.
+     * 
+     * @param directory <code>{@link File }</code>: The directory you wish to delete.
+     * @throws IOException If something goes wrong trying to delete the files.
+     */
+    public static void deleteDirectory( File directory ) throws IOException {
+        if ( directory.isDirectory() )
+            for ( File i : directory.listFiles() )
+                deleteDirectory(i);
+        Files.delete(directory.toPath());
+    }
+
+    /*
+     * =============================================== GETTERS AND SETTERS ==============================================
+     */
+    /**
+     * Gets the App Data file for this operating system.
+     *
+     * @return <code>{@link File }</code>: The App Data file for this OS.
+     * @throws IOException If the OS is unknown.
+     */
+    public static File getAppDataFile() throws IOException {
         String OS = (System.getProperty("os.name")).toUpperCase();
         if ( OS.contains("WIN") )
             return new File(System.getenv("AppData"));
@@ -60,12 +84,20 @@ public class CreaFiles {
         else if ( OS.contains("NUX") )
             return new File(System.getProperty("user.home"));
         else
-            throw new UnsupportedOperationException("Can't figure out OS.");
+            throw new IOException("Unkown operating system!");
     }
 
-    /*
-     * =============================================== GETTERS AND SETTERS ==============================================
+    /**
+     * Gets the Temp file for this operating system.
+     *
+     * @return <code>{@link File }</code>: The Temp file for this OS.
+     * @throws IOException If the OS is unknown.
      */
+    public static File getTempFile() throws IOException {
+        String OS = (System.getProperty("os.name")).toUpperCase();
+        throw new UnsupportedOperationException("TODO: THIS");
+    }
+
     /*
      * ================================================ VISUAL FUNCTIONS ================================================
      */
