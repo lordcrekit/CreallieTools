20e2a0d6fb46cf6737fdf7bf4c9d8248618aea58
lordcrekit <lordcrekit@gmail.com>
2016-05-26 14:17:22 -0400

    Fixed a big bug and added Path saving.

--- src/Creallie/Tools/util/CreaProperties.java
+++ src/Creallie/Tools/util/CreaProperties.java
@@ -24,6 +24,8 @@
 package Creallie.Tools.util;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.text.SimpleDateFormat;
 import java.util.Collections;
 import java.util.Date;
@@ -32,7 +34,8 @@ import java.util.Map;
 import java.util.Properties;
 
 /**
- * CreaProperties is a replacement for the {@link Properties } class. Thread safe. It can actually be used to quickly load, add to, and save files.
+ * CreaProperties is a replacement for the {@link Properties } class. Thread safe. It can actually be used to quickly load, add
+ * to, and save files.
  *
  * @author William A Norman (Lordcrekit@gmail.com, normanwi@msu.edu)
  */
@@ -41,7 +44,7 @@ public class CreaProperties {
     /*
      * ================================================ MEMBER VARIABLES ================================================
      */
-    private Map<String, String> mProperties;
+    private Map<String, String> properties;
 
     /*
      * ================================================== CONSTRUCTORS ==================================================
@@ -50,29 +53,29 @@ public class CreaProperties {
      * Default constructor for CreaProperties.
      */
     public CreaProperties() {
-        mProperties = Collections.synchronizedMap(new HashMap<>());
+        this.properties = Collections.synchronizedMap(new HashMap<>());
     }
 
     /**
      * Copy constructor for CreaProperties.
      *
-     * @param orig <code>{@link CreaProperties }</code>: The object to copy.
+     * @param orig The object to copy.
      */
     public CreaProperties( CreaProperties orig ) {
-        mProperties = new HashMap<>();
+        this.properties = new HashMap<>();
         for ( String key : orig.getProperties().keySet() )
-            mProperties.put(key, orig.getProperties().get(key));
+            this.properties.put(key, orig.getProperties().get(key));
     }
 
     /**
      * Constructs a CreaProperties from a Properties object.
      *
-     * @param props <code>{@link Properties }</code>: The Properties object to copy.
+     * @param props The Properties object to copy.
      */
     public CreaProperties( Properties props ) {
-        mProperties = new HashMap<>();
+        this.properties = new HashMap<>();
         for ( Object key : props.keySet() )
-            mProperties.put(key.toString(), props.get(key).toString());
+            this.properties.put(key.toString(), props.get(key).toString());
     }
 
     /*
@@ -81,11 +84,10 @@ public class CreaProperties {
     /**
      * Gets a Key-Value Map of the properties.
      *
-     * @return <code>{@link Map }&lt;{@link String }, {@link String }&gt;</code>: A key-value paired map.
+     * @return A key-value paired map.
      */
     public Map<String, String> getProperties() {
-        System.out.println(this.toString());
-        return mProperties;
+        return this.properties;
     }
 
     /**
@@ -112,12 +114,12 @@ public class CreaProperties {
     /**
      * Sets the given key to the given value.
      *
-     * @param key <code>{@link String }</code>: The key that is being set.
-     * @param value <code>{@link String }</code>: The value being saved to the key.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param key   The key that is being set.
+     * @param value The value being saved to the key.
+     * @return Pointer back to this object.
      */
     public CreaProperties setProperty( String key, String value ) {
-        mProperties.put(key, value);
+        this.properties.put(key, value);
         return this;
     }
 
@@ -127,9 +129,9 @@ public class CreaProperties {
     /**
      * Saves the properties to the given OutputStream. The file will also contain a timestamp of when it was saved.
      *
-     * @param outstream <code>{@link OutputStream }</code>: The OutputStream write to.
-     * @param comments  <code>{@link String }</code>: Comments to save with the file.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param outstream The OutputStream write to.
+     * @param comments  Comments to save with the file.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to save.
      */
     public CreaProperties save( OutputStream outstream, String comments ) throws IOException {
@@ -140,9 +142,9 @@ public class CreaProperties {
     /**
      * Saves the properties using the given Writer. The file will also contain a timestamp of when it was saved.
      *
-     * @param writer   <code>{@link Writer }</code>: The Writer use.
-     * @param comments <code>{@link String }</code>: Comments to save with the file.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param writer   The Writer use.
+     * @param comments Comments to save with the file.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to save.
      */
     public CreaProperties save( Writer writer, String comments ) throws IOException {
@@ -153,11 +155,12 @@ public class CreaProperties {
     /**
      * Saves the properties to the given File. The file will also contain a timestamp of when it was saved.
      *
-     * @param file     <code>{@link File }</code>: The file to save to.
-     * @param comments <code>{@link String }</code>: Comments to save with the file.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param file     The file to save to.
+     * @param comments Comments to save with the file.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to save.
      */
+    @Deprecated
     public CreaProperties save( File file, String comments ) throws IOException {
         try ( BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(file))) ) {
             save(writer, comments);
@@ -166,10 +169,25 @@ public class CreaProperties {
     }
 
     /**
+     * Saves the properties to the given Path. File created will contain timestamp.
+     *
+     * @param path     Path to the file to save to.
+     * @param comments Comments that will be saved with the file.
+     * @return Pointer back to this object.
+     * @throws IOException If something goes wrong while attempting to save.
+     */
+    public CreaProperties save( Path path, String comments ) throws IOException {
+        try ( BufferedWriter writer = Files.newBufferedWriter(path) ) {
+            save(writer, comments);
+        }
+        return this;
+    }
+
+    /**
      * Loads the properties from a given InputStream.
      *
-     * @param instream <code>{@link InputStream }</code>: The InputStream to use.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param instream The InputStream to use.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to load.
      */
     public CreaProperties load( InputStream instream ) throws IOException {
@@ -180,8 +198,8 @@ public class CreaProperties {
     /**
      * Loads the properties from a given Reader.
      *
-     * @param reader <code>{@link Reader }</code>: The Reader to use.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param reader The Reader to use.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to load.
      */
     public CreaProperties load( Reader reader ) throws IOException {
@@ -192,12 +210,27 @@ public class CreaProperties {
     /**
      * Loads the properties from the given File.
      *
-     * @param file <code>{@link File }</code>: The file to read from.
-     * @return <code>{@link CreaProperties }</code>: Pointer back to this object.
+     * @param file The file to read from.
+     * @return Pointer back to this object.
      * @throws IOException If something goes wrong while attempting to load.
      */
+    @Deprecated
     public CreaProperties load( File file ) throws IOException {
         try ( BufferedReader reader = new BufferedReader(new FileReader(file)) ) {
+            load(reader);
+        }
+        return this;
+    }
+
+    /**
+     * Loads the properties from the given filepath.
+     *
+     * @param path Path to the file to read from.
+     * @return Pointer back to this object.
+     * @throws IOException If something goes wrong while attempting to read the file.
+     */
+    public CreaProperties load( Path path ) throws IOException {
+        try ( BufferedReader reader = Files.newBufferedReader(path) ) {
             load(reader);
         }
         return this;
@@ -209,11 +242,11 @@ public class CreaProperties {
     /**
      * Just a String representation of the internal Map of properties.
      *
-     * @return <code>{@link String }</code>: Representation of this object.
+     * @return Representation of this object.
      */
     @Override
     public String toString() {
-        return mProperties.toString();
+        return this.properties.toString();
     }
 
     /*
@@ -231,8 +264,8 @@ public class CreaProperties {
             for ( String i : comments.split("\n") )
                 writer.write('#' + i + "\n");
         writer.write(new SimpleDateFormat("'#Created on 'yyyy-MM-dd'T'HH.mm.ss'GMT'Z'\n'").format(new Date()));
-        for ( String key : mProperties.keySet() )
-            writer.write(key + "=" + mProperties.get(key) + "\n");
+        for ( String key : this.properties.keySet() )
+            writer.write(key + "=" + this.properties.get(key) + "\n");
         writer.flush();
     }
 
@@ -243,12 +276,14 @@ public class CreaProperties {
      * @throws IOException
      */
     private void load( BufferedReader reader ) throws IOException {
-        mProperties = Collections.synchronizedMap(new HashMap<>());
+        this.properties = Collections.synchronizedMap(new HashMap<>());
         while ( reader.ready() ) {
             String no_comments = reader.readLine().split("#")[0];
             int eqind = no_comments.indexOf('=');
             if ( eqind != -1 )
-                mProperties.put(no_comments.substring(0, eqind), no_comments.length() > eqind ? no_comments.substring(eqind + 1, no_comments.length()) : null);
+                this.properties.put(no_comments.substring(0, eqind), no_comments.length() > eqind
+                        ? no_comments.substring(eqind + 1, no_comments.length())
+                        : null);
         }
     }
 }
