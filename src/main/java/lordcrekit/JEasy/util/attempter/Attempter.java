66ba801ab1d2df3b905bccb98d31ac804f71644d
William Norman <lordcrekit@gmail.com>
2017-07-11 21:23:33 -0400

    Codeup of attempter. Probably should replace this with Futures...

--- src/main/java/lordcrekit/JEasy/util/attempter/Attempter.java
+++ src/main/java/lordcrekit/JEasy/util/attempter/Attempter.java
@@ -32,78 +32,98 @@ import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 
 /**
- * The Attempter class is a convenience class to assist with running blocks of code with no guarantee of completion.
+ * The Attempter class is a convenience class to assist with running blocks of
+ * code with no guarantee of completion.
  *
  * @author William A. Norman (LordCrekit@gmail.com, normanwi@msu.edu)
  */
 public class Attempter {
 
-    /*
-     * ================================================ PRIMARY FUNCTIONS ===============================================
-     */
-    /**
-     * Attempts to run the given Callable with the set number of attempts before it gives up.
-     *
-     * @param <T> <code>T</code>: The return type of your Callable.
-     * @param callable <code>{@link Callable }</code>: The Callable that will be run.
-     * @param attempts <code>int</code>: The number of times to attempt.
-     * @return <code>T</code>: Whatever your Callable returns.
-     * @throws AllAttemptsFailedException If every attempt is failed.
-     */
-    public static <T> T attemptMultiple( Callable<T> callable, int attempts ) throws AllAttemptsFailedException {
-        Exception lastEx = null;
-        for ( int counter = 0; counter < attempts; counter++ )
-            try {
-                return callable.call();
-            } catch ( RuntimeException ex ) {
-                lastEx = ex;
-            } catch ( Exception ex ) {
-                lastEx = ex;
-            }
-        throw new AllAttemptsFailedException("All attempts failed.", lastEx);
-    }
+  /**
+   * Attempts to run the given Callable with the set number of attempts before
+   * it gives up.
+   *
+   * @param <T>
+   *     The return type of your Callable.
+   * @param callable
+   *     The Callable that will be run.
+   * @param attempts
+   *     The number of times to attempt.
+   * @return Whatever your Callable returns.
+   * @throws AllAttemptsFailedException
+   *     If every attempt is failed.
+   */
+  public static <T> T attemptMultiple(Callable<T> callable, int attempts) throws AllAttemptsFailedException {
+    Exception lastEx = null;
+    for (int counter = 0; counter < attempts; counter++)
+      try {
+        return callable.call();
+      } catch (RuntimeException ex) {
+        lastEx = ex;
+      } catch (Exception ex) {
+        lastEx = ex;
+      }
+    throw new AllAttemptsFailedException("All attempts failed.", lastEx);
+  }
 
-    /**
-     * Attempts to run the given Callable within the set timeout time before it gives up. Always uses milliseconds.
-     *
-     * @param <T> <code>T</code>: The return type of your Callable.
-     * @param callable <code>{@link Callable }</code>: The Callable that will be run.
-     * @param timeout <code>long</code>: The number of milliseconds to wait for.
-     * @param cancelThread <code>boolean</code>: If the thread should be shut down on failure.
-     * @return
-     * @throws TimeoutException
-     * @throws ExecutionException
-     */
-    public static <T> T attemptTimeout( Callable<T> callable, long timeout, boolean cancelThread ) throws TimeoutException, ExecutionException {
-        return attemptTimeout(callable, timeout, TimeUnit.MILLISECONDS, cancelThread);
-    }
+  /**
+   * Attempts to run the given Callable within the set timeout time before it
+   * gives up. Always uses milliseconds.
+   *
+   * @param <T>
+   *     The return type of your Callable.
+   * @param callable
+   *     The Callable that will be run.
+   * @param timeout
+   *     The number of milliseconds to wait for.
+   * @param cancelThread
+   *     If the thread should be shut down on failure.
+   * @return Whatever the callable returns.
+   * @throws TimeoutException
+   *     If the time runs out before the callable returns.
+   * @throws ExecutionException
+   *     If the callable throws an exception.
+   */
+  public static <T> T attemptTimeout(Callable<T> callable, long timeout, boolean cancelThread)
+      throws TimeoutException, ExecutionException {
 
-    /**
-     * Attempts to run the given Callable within the set timeout time before it gives up. Allows you to specify the time unit being used.
-     *
-     * @param <T> <code>T</code>: The return type of your Callable.
-     * @param callable <code>{@link Callable }</code>: The Callable that will be run.
-     * @param timeout <code>long</code>: The number of specified TimeUnits to wait for.
-     * @param timeUnit <code>{@link TimeUnit }</code>: The TimeUnit that is being used.
-     * @param cancelThread <code>boolean</code>: If the thread should be shut down on failure.
-     * @return
-     * @throws TimeoutException
-     * @throws ExecutionException
-     */
-    public static <T> T attemptTimeout( Callable<T> callable, long timeout, TimeUnit timeUnit, boolean cancelThread ) throws TimeoutException, ExecutionException {
-        try {
-            final ExecutorService executor = Executors.newSingleThreadExecutor();
-            final Future<T> future = executor.submit(callable);
-            try {
-                return future.get(timeout, timeUnit);
-            } catch ( TimeoutException e ) {
-                future.cancel(cancelThread);
-                throw e;
-            } finally {
-                executor.shutdown();
-            }
-        } catch ( InterruptedException ex ) {
-            throw new ExecutionException("Interrupted.", ex);
-        }
+    return attemptTimeout(callable, timeout, TimeUnit.MILLISECONDS, cancelThread);
+  }
+
+  /**
+   * Attempts to run the given Callable within the set timeout time before it
+   * gives up. Allows you to specify the time unit being used.
+   *
+   * @param <T>
+   *     The return type of your Callable.
+   * @param callable
+   *     The Callable that will be run.
+   * @param timeout
+   *     The number of specified TimeUnits to wait for.
+   * @param timeUnit
+   *     The TimeUnit that is being used.
+   * @param cancelThread
+   *     If the thread should be shut down on failure.
+   * @return Whatever the callable returns.
+   * @throws TimeoutException
+   *     If time runs out before the callable returns.
+   * @throws ExecutionException
+   *     If the callable throws an exception.
+   */
+  public static <T> T attemptTimeout(Callable<T> callable, long timeout, TimeUnit timeUnit, boolean cancelThread) throws TimeoutException, ExecutionException {
+    try {
+      final ExecutorService executor = Executors.newSingleThreadExecutor();
+      final Future<T> future = executor.submit(callable);
+      try {
+        return future.get(timeout, timeUnit);
+      } catch (TimeoutException e) {
+        future.cancel(cancelThread);
+        throw e;
+      } finally {
+        executor.shutdown();
+      }
+    } catch (InterruptedException ex) {
+      throw new ExecutionException("Interrupted.", ex);
     }
+  }
 }
